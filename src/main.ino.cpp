874291fcb3e54e05610968f920d040837c74868f
Marek Küthe <m.k@mk16.de>
2021-01-31 19:07:48 +0100

    Define function call_alarm && program function mode_clock

--- src/main.ino.cpp
+++ src/main.ino.cpp
@@ -1,3 +1,6 @@
+# 1 "/tmp/user/1000/tmps7_jxmg9"
+#include <Arduino.h>
+# 1 "/home/marek/NetBeansProjects/Wecker/src/main.ino"
 
 #include <Arduino.h>
 
@@ -9,7 +12,7 @@
 #include "U8x8lib.h"
 #include "oled_input.hpp"
 
-/* pins */
+
 #define BUTTONS_ROW_1 A1
 #define BUTTONS_ROW_2 A0
 
@@ -23,7 +26,7 @@
 
 #define BUZZER 3
 
-/* attributes */
+
 
 #define BUTTON_TIMEOUT 500
 
@@ -31,12 +34,12 @@
 #define ALARM_BUZZER_FREQ_MIN 300
 #define ALARM_BUZZER_FREQ_MAX 700
 
-/* I2C addresses */
+
 #define EEPROM_ADDRESS 0b1010001
 
-/* enums */
+
 enum BUTTON {
-    /*0*//*1*//* 2 */   /* 3  */    /*4*/ /*5*/ /* 6 */
+
     CFK, NEXT, OK_EDIT, CHANGE_MODE, PLUS, MINUS, NONE
 };
 
@@ -52,7 +55,7 @@ enum TIME_EDIT {
     HOUR, MINUTE, COMPLETE
 };
 
-/* functions */
+
 BUTTON read_buttons();
 void next_mode();
 void next_temperature_unit();
@@ -70,11 +73,11 @@ void get_day_from_user(unsigned *);
 void check_alarms();
 void call_alarm();
 
-/* variables */
-MODE current_mode = ALARM_CLOCK; //MAIN;
+
+MODE current_mode = ALARM_CLOCK;
 UNIT_OF_TEMPERATURE unit_for_temperature = CELSIUS;
 float dht_result[2] = {0, 0};
-unsigned long th_update_intervall = 500;  /* temp humi update intervall */
+unsigned long th_update_intervall = 500;
 unsigned long th_timestamp = 0;
 bool led_red_state = false;
 
@@ -82,42 +85,47 @@ unsigned alarms[NUM_OF_ALARMS][2];
 bool alarms_complete[NUM_OF_ALARMS];
 bool alarms_on[NUM_OF_ALARMS];
 
-/* objects */
+
 TM1637Display tm1637_display(SEGMENT_DISPLAY_CLK, SEGMENT_DISPLAY_DIO);
 segment_display secondary_display(&tm1637_display, 88, 88, 0, true);
 DS1307 rtc;
 eeprom_24c256 eeprom(EEPROM_ADDRESS);
 DHT dht(TEMP_HUMI, DHT11);
 U8X8_SSD1306_128X64_NONAME_HW_I2C oled(U8X8_PIN_NONE);
-
-/*
- * eeprom notes
- * 0 - Clock 1 - Hour
- * 1 - Clock 1 - Minute
- * 2 - Clock 1 - On/Off
- * 3 - Clock 2 - Hour
- * 4 - Clock 2 - Minute
- * 5 - Clock 2 - On/Off
- *
-*/
-
+# 104 "/home/marek/NetBeansProjects/Wecker/src/main.ino"
+void setup();
+void loop();
+inline void next_mode();
+inline void next_temperature_unit();
+void mode_main(BUTTON pressed_button);
+void mode_set_time(BUTTON pressed_button);
+void mode_alarm_clock(BUTTON pressed_button);
+inline void update_temp_humi();
+inline void change_to_main_mode();
+inline void change_red_led_state();
+void get_time_from_user(unsigned * hour, unsigned * minute, char colon, int max1, int max2);
+void get_seconds_from_user(unsigned * seconds, unsigned startpos, int max, unsigned startval);
+inline void get_year_from_user(unsigned * year);
+inline void get_date_from_user(unsigned * mon, unsigned * day);
+void get_day_from_user(unsigned * day);
+#line 104 "/home/marek/NetBeansProjects/Wecker/src/main.ino"
 void setup()
 {
     Serial.begin(9600);
     Serial.println("Hello!");
-    
+
     pinMode(BUTTONS_ROW_1, INPUT);
     pinMode(BUTTONS_ROW_2, INPUT);
-  
+
     pinMode(SEGMENT_DISPLAY_DIO, OUTPUT);
     pinMode(SEGMENT_DISPLAY_CLK, OUTPUT);
-    
+
     pinMode(GREEN_LED, OUTPUT);
     digitalWrite(GREEN_LED, LOW);
-    
+
     pinMode(RED_LED, OUTPUT);
     digitalWrite(RED_LED, HIGH);
-    
+
     rtc.begin();
     eeprom.init();
     dht.begin();
@@ -125,23 +133,23 @@ void setup()
     oled.setBusClock(100000);
     oled.begin();
     oled.setFont(u8x8_font_victoriamedium8_r);
-    
+
     update_temp_humi();
     th_timestamp = millis();
-    
+
     for (size_t i = 0; i < NUM_OF_ALARMS; i++)
     {
         alarms_complete[i] = false;
-        
+
         byte data[3];
-        
+
         eeprom.read(3 * i, data, 3);
         delay(10);
-        
+
         Serial.println(data[0]);
         Serial.println(data[1]);
         Serial.println(data[2]);
-        
+
         alarms[i][0] = static_cast<unsigned>(data[0]);
         alarms[i][1] = static_cast<unsigned>(data[1]);
         alarms_on[i] = static_cast<bool>(data[2]);
@@ -151,27 +159,27 @@ void setup()
 void loop()
 {
     BUTTON pressed_button = read_buttons();
-    
+
     if (pressed_button != NONE)
     {
         digitalWrite(GREEN_LED, HIGH);
     }
-    
+
     change_red_led_state();
-    
+
     if ( (th_timestamp - millis()) >= th_update_intervall && current_mode == MAIN )
     {
         update_temp_humi();
         th_timestamp = millis();
     }
     rtc.getTime();
-    
-    /* code */
+
+
     if (pressed_button == CHANGE_MODE)
     {
         next_mode();
     }
-    
+
     if (current_mode == MAIN)
     {
         mode_main(pressed_button);
@@ -184,20 +192,20 @@ void loop()
     {
         mode_alarm_clock(pressed_button);
     }
-    
+
     secondary_display.set_number(rtc.hour, 0);
     secondary_display.set_number(rtc.minute, 1);
     secondary_display.refresh_display();
-    
+
     check_alarms();
-    
+
     if (pressed_button != NONE)
     {
         digitalWrite(GREEN_LED, LOW);
         unsigned long timestamp = millis();
         while ( (millis() - timestamp) <= BUTTON_TIMEOUT)
         {
-            /* code while wait */
+
         }
     }
 }
@@ -206,8 +214,8 @@ BUTTON read_buttons()
 {
     int row_1 = analogRead(BUTTONS_ROW_1);
     int row_2 = analogRead(BUTTONS_ROW_2);
-    
-    /* eval row 1 */
+
+
     if (row_1 >= 670 && row_1 <= 683)
     {
         return OK_EDIT;
@@ -220,8 +228,8 @@ BUTTON read_buttons()
     {
         return CFK;
     }
-    
-    /* eval row 2 */
+
+
     if (row_2 >= 670 && row_2 <= 683)
     {
         return MINUS;
@@ -234,7 +242,7 @@ BUTTON read_buttons()
     {
         return CHANGE_MODE;
     }
-    
+
     return NONE;
 }
 
@@ -264,10 +272,10 @@ inline void next_temperature_unit()
 
 void mode_main(BUTTON pressed_button)
 {
-    /* reactions on buttons */
+
     if (pressed_button == PLUS || pressed_button == MINUS)
     {
-        /* Dimming the display */
+
         uint8_t light = secondary_display.get_light();
         light += (pressed_button == MINUS ? -1 : +1);
         if ( ! (light > 7) )
@@ -277,7 +285,7 @@ void mode_main(BUTTON pressed_button)
     }
     else if (pressed_button == OK_EDIT)
     {
-        /* Change display state (on/off) */
+
         secondary_display.turn();
     }
     else if (pressed_button == NEXT)
@@ -288,51 +296,51 @@ void mode_main(BUTTON pressed_button)
     {
         next_temperature_unit();
     }
-    
-    /* print time and date to the oled display */
+
+
     oled.setFont(u8x8_font_profont29_2x3_r);
     oled.setCursor(0, 0);
-    /* print hours */
+
     if (rtc.hour < 10)
         oled.print("0");
     oled.print(rtc.hour);
-    
+
     oled.print(":");
-    
-    /* print minutes */
+
+
     if (rtc.minute < 10)
         oled.print("0");
     oled.print(rtc.minute);
 
     oled.setFont(u8x8_font_7x14B_1x2_r);
     oled.setCursor(10, 1);
-    /* print seconds */
+
     oled.print(":");
     if (rtc.second < 10)
         oled.print("0");
     oled.print(rtc.second);
     oled.print(" ");
 
-    /* print date */
+
     oled.setCursor(1, 3);
-    
-    /* print dayOfMonth */
+
+
     if (rtc.dayOfMonth < 10)
         oled.print("0");
     oled.print(rtc.dayOfMonth);
-    
+
     oled.print("/");
-    
-    /* print month */
+
+
     if (rtc.month < 10)
         oled.print("0");
     oled.print(rtc.month);
-    
-    /* print year */
+
+
     oled.print("/");
     oled.print(rtc.year + 2000);
-    
-    /* print dayOfWeek */
+
+
     oled.print(" ");
     switch(rtc.dayOfWeek)
     {
@@ -358,14 +366,14 @@ void mode_main(BUTTON pressed_button)
             oled.print("SUN");
             break;
     }
-    
-    
-    /* print temp and humi to the oled display */
+
+
+
     oled.setCursor(1, 6);
     oled.print(dht_result[0]);
     oled.print("%");
     oled.print(" ");
-    
+
     if (unit_for_temperature == CELSIUS)
     {
         oled.print(dht_result[1]);
@@ -389,7 +397,7 @@ void mode_set_time(BUTTON pressed_button)
     oled.setFont(u8x8_font_7x14B_1x2_r);
     oled.setCursor(0, 1);
     oled.print(" Would you like\n  to set the\n    clock?");
-    
+
     if (pressed_button == OK_EDIT)
     {
         unsigned hour;
@@ -399,46 +407,46 @@ void mode_set_time(BUTTON pressed_button)
         unsigned mon;
         unsigned day;
         unsigned year;
-        
+
         oled.clear();
         get_time_from_user(&hour, &minute);
-        
+
         oled.clear();
         get_seconds_from_user(&seconds);
-        
+
         oled.clear();
         get_day_from_user(&day_of_week);
-        
+
         oled.clear();
         get_date_from_user(&mon, &day);
-        
+
         oled.clear();
         get_year_from_user(&year);
-        
+
         oled.clear();
-        
+
         if (static_cast<int>(year) - 2000 < 0)
             year = 0;
-        //year -= 2000;
-        
+
+
         if (mon == 0)
             mon = 1;
-        
+
         if (day == 0)
             day = 1;
-        
+
         oled.setFont(u8x8_font_7x14B_1x2_r);
         oled.setCursor(0, 1);
         oled.print("   Should the\n    time be\n    set now?");
         while(read_buttons() == NONE);
-        
-        //rtc.stopClock();
+
+
         rtc.fillByYMD(year, mon, day);
         rtc.fillByHMS(hour, minute, seconds);
         rtc.fillDayOfWeek(day_of_week);
         rtc.setTime();
-        //rtc.startClock();
-        
+
+
         oled.clear();
     }
 }
@@ -447,20 +455,20 @@ void mode_alarm_clock(BUTTON pressed_button)
 {
     oled.setFont(u8x8_font_7x14B_1x2_r);
     oled.setCursor(0, 1);
-    
+
     for (size_t i = 0; i < NUM_OF_ALARMS; i++)
     {
         oled.print(" ");
         if (alarms[i][0] < 10)
             oled.print("0");
         oled.print(alarms[i][0]);
-       
+
         oled.print(":");
-        
+
         if (alarms[i][1] < 10)
             oled.print("0");
         oled.print(alarms[i][1]);
-        
+
         oled.print("  ");
         oled.print(alarms_on[i] ? "ON" : "OFF");
         oled.print(" \n");
@@ -487,20 +495,20 @@ void get_time_from_user(unsigned * hour, unsigned * minute, char colon, int max1
 {
     int tmp_hour = 0;
     int tmp_minute = 0;
-    
+
     int edit_mode = 1;
-    /*
-     * -1 = complete
-     * 1 = hour
-     * 2 = minute
-     */
-    
+
+
+
+
+
+
     oled.setFont(u8x8_font_profont29_2x3_r);
-    
+
     while (edit_mode != -1)
     {
         BUTTON pressed_button = read_buttons();
-        
+
         if (pressed_button == NEXT)
         {
             edit_mode = (edit_mode == 1 ? 2 : 1);
@@ -518,41 +526,41 @@ void get_time_from_user(unsigned * hour, unsigned * minute, char colon, int max1
                 if (tmp_hour < 0 || tmp_hour > max1)
                     tmp_hour -= val;
             }
-            else /* if (edit_mode == 2) */
+            else
             {
                 tmp_minute += val;
                 if (tmp_minute < 0 || tmp_minute > max2)
                     tmp_minute -= val;
             }
         }
-            
+
         oled.setCursor(3, 2);
         if (edit_mode == 1)
             oled.inverse();
-        
+
         if (tmp_hour < 10)
             oled.print("0");
         oled.print(tmp_hour);
-        
+
         if (edit_mode == 1)
             oled.noInverse();
-        
+
         oled.print(colon);
-        
+
         if (edit_mode == 2)
             oled.inverse();
-        
+
         if (tmp_minute < 10)
             oled.print("0");
         oled.print(tmp_minute);
-        
+
         if (edit_mode == 2)
             oled.noInverse();
-        
+
         if (pressed_button != NONE)
             delay(BUTTON_TIMEOUT);
     }
-    
+
     * hour = tmp_hour;
     * minute = tmp_minute;
 }
@@ -560,16 +568,16 @@ void get_time_from_user(unsigned * hour, unsigned * minute, char colon, int max1
 void get_seconds_from_user(unsigned * seconds, unsigned startpos, int max, unsigned startval)
 {
     int tmp_seconds = startval;
-    
+
     bool complete = false;
-    
+
     oled.setFont(u8x8_font_profont29_2x3_r);
-    
+
     oled.inverse();
     while (! complete)
     {
         BUTTON pressed_button = read_buttons();
-        
+
         if (pressed_button == OK_EDIT)
         {
             complete = true;
@@ -581,18 +589,18 @@ void get_seconds_from_user(unsigned * seconds, unsigned startpos, int max, unsig
             if (tmp_seconds < 0 || tmp_seconds > max)
                 tmp_seconds -= val;
         }
-            
+
         oled.setCursor(startpos, 2);
-        
+
         if (tmp_seconds < 10)
             oled.print("0");
         oled.print(tmp_seconds);
-        
+
         if (pressed_button != NONE)
             delay(BUTTON_TIMEOUT);
     }
     oled.noInverse();
-    
+
     * seconds = tmp_seconds;
 }
 
@@ -609,16 +617,16 @@ inline void get_date_from_user(unsigned * mon, unsigned * day)
 void get_day_from_user(unsigned * day)
 {
     int tmp_day = 1;
-    
+
     bool complete = false;
-    
+
     oled.setFont(u8x8_font_profont29_2x3_r);
-    
+
     oled.inverse();
     while (! complete)
     {
         BUTTON pressed_button = read_buttons();
-        
+
         if (pressed_button == OK_EDIT)
         {
             complete = true;
@@ -630,9 +638,9 @@ void get_day_from_user(unsigned * day)
             if (tmp_day < 0 || tmp_day > 7)
                 tmp_day -= val;
         }
-            
+
         oled.setCursor(3, 2);
-        
+
         switch(tmp_day)
         {
             case MON:
@@ -657,12 +665,12 @@ void get_day_from_user(unsigned * day)
                 oled.print("SUN");
                 break;
         }
-        
+
         if (pressed_button != NONE)
             delay(BUTTON_TIMEOUT);
     }
     oled.noInverse();
-    
+
     * day = tmp_day;
 }
 
@@ -674,7 +682,7 @@ void check_alarms()
         {
             if (rtc.hour == alarms[i][0] && rtc.minute == alarms[i][1] && alarms_complete[i] == false)
             {
-                /* call alarm */
+
                 alarms_complete[i] = true;
                 call_alarm();
             }
@@ -690,50 +698,50 @@ void call_alarm()
 {
     oled.clear();
     oled.setFont(u8x8_font_profont29_2x3_r);
-    
+
     bool inverse = false;
     bool complete = false;
     int freq = ALARM_BUZZER_FREQ_MIN;
-    
+
     while (! complete)
     {
         unsigned long timestamp = millis();
-        
-        /* Alarm on Oled */
+
+
         oled.setCursor(1, 1);
-        
+
         if (inverse)
             oled.inverse();
-        
-        /* print hours */
+
+
         if (rtc.hour < 10)
             oled.print("0");
         oled.print(rtc.hour);
 
         oled.print(":");
 
-        /* print minutes */
+
         if (rtc.minute < 10)
             oled.print("0");
         oled.print(rtc.minute);
-        
+
         if (inverse)
             oled.noInverse();
-        
+
         inverse = ! inverse;
-        
-        /* Alarm on Second display */
+
+
         secondary_display.turn();
         secondary_display.refresh_display();
-        
-        /* Buzzer */
+
+
         tone(BUZZER, freq);
         freq += 50;
         if (freq > ALARM_BUZZER_FREQ_MAX)
         {
             freq = ALARM_BUZZER_FREQ_MIN;
         }
-        
+
         while ( (millis() - timestamp) <= 250 )
         {
             if (read_buttons() != NONE)
